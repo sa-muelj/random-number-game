aa30ac8d12204e54bdf4094c3d3fb6bd6dbe193f
sa-muelj <samuel.jones@ada.ac.uk>
2022-11-04 12:26:47 +0000

    Added differenceFromAnswer function

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function generateRandomNumber(){
 
   return(Math.floor(Math.random() * 10));
 }
+
+function differenceFromAnswer(guess, answer){
+  answer = differenceFromAnswer()
+  if (guess === answer){
+    return "Correct"
+  } else if (guess > answer){
+    return "Too high"
+  } else {
+    return "Too low"
+  }
+}
